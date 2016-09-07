660db3a976e2fb4fc8b9176d174d6ed444992bf8
Betsy Haibel <betsy.haibel@gmail.com>
2016-09-06 23:31:18 -0400

    produce a random number of stars

--- src/star_set.rs
+++ src/star_set.rs
@@ -1,24 +1,39 @@
+extern crate rand;
+
 use std::vec::Vec;
+use self::rand::{thread_rng, sample};
+use self::rand::distributions::{IndependentSample, Range};
 
 pub struct Point {
-  x: f32,
-  y: f32
+    x: f32,
+    y: f32
 }
 
-pub fn stars(count: i32) -> Vec<Point> {
-  let mut stars = Vec::new();
-  for x in 0..(count) {
-    stars.push(Point { x: 0.0, y: 0.0 });
-  }
-  stars
+pub fn stars() -> Vec<Point> {
+    let mut rng = thread_rng();
+    let count = Range::new(6, 13).ind_sample(&mut rng);
+    
+    (0..count)
+        .map(|_| Point { x: 0.0, y: 0.0 })
+        .collect::<Vec<Point>>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::stars;
+    use super::Point;
 
     #[test]
-    fn count_stars() {
-        assert_eq!(4, stars(4).len());
+    fn test_star_properties() {
+        for _ in 0..100 {
+            let subject = stars();
+
+            count_stars(subject);
+        }
+    }
+
+    fn count_stars(subject: Vec<Point>) {
+        assert!(6 <= subject.len());
+        assert!(13 >= subject.len());
     }
 }
